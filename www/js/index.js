7c911c0d5bff99e4a3378ad6a487c534a6200fe2
lokyoung <lokyoung@outlook.com>
2016-04-12 17:48:12 +0800

    feat: test just for read

--- www/js/index.js
+++ www/js/index.js
@@ -17,211 +17,127 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        var pluginElement = parentElement.querySelector('.plugin');
-        var batteryElement = parentElement.querySelector('.battery');
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    var pluginElement = parentElement.querySelector('.plugin');
+    var read_result = document.getElementById('read_result');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
 
-        var BLOCK = 4;
+    var BLOCK = 4;
 
-        var read_uid_success =  function(result){
-              alert("UID: " + JSON.stringify(result))
-        }
-
-        var read_uid_failure =  function(result){
-              alert("error UID: " + JSON.stringify(result))
-        }
-        var read_success =  function(result){
-              alert("data: " + JSON.stringify(result))
-        }
-
-        var read_failure =  function(result){
-              alert("error data: " + JSON.stringify(result))
-        }
-        var write_success =  function(result){
-              alert("write data: " + JSON.stringify(result))
-        }
-
-        var write_failure =  function(result){
-              alert("write error data: " + JSON.stringify(result))
-        }
-
-        var _cb =  function(result){
-              alert("data: " + JSON.stringify(result))
-        }
-
-        var dataBlock = document.getElementById("data_block");
-        var readButton = document.getElementById("read_data");
-        readButton.addEventListener('click', function() {
-          //if(keyA.value.length > 0){
-            //ACR.authenticateWithKeyA(4,keyA.value, function(){
-                //ACR.readData(BLOCK,read_success,read_failure);
-              //}, function(){
-                //alert('authenticate fail');
-                //return;
-              //}
-              //)
-          //}else{
-            ACR.readData(parseInt(dataBlock.value,10), passwordInput.value, read_success, read_failure);
-          //}
-        });
-
-        var displayButton = document.getElementById("display");
-        var displayInput = document.getElementById("input_display");
-        var clearButton = document.getElementById("clear");
-
-        displayButton.addEventListener('click', function() {
-          ACR.display(displayInput.value,{bold: true , font:1,x:0},function(r){},function(r){});
-          ACR.display(displayInput.value,{bold: false, font:3,x:2, y:4},function(r){},function(r){});
-          ACR.display(displayInput.value,{bold: false, font:3,x:3, y:6},function(r){},function(r){});
-        });
-
-        clearButton.addEventListener('click', function() {
-          ACR.clearLCD(function(r){},function(r){});
-        });
-
-        var isReadyButton = document.getElementById("is_ready");
-        var writeButton = document.getElementById("write_data");
-        var writeInput = document.getElementById("write_data_input");
-        var passwordInput = document.getElementById("password_input");
-        var oldPasswordInput = document.getElementById("old_password_input");
-        var initNtagButton = document.getElementById("init_ntag");
-        var getVersion = document.getElementById("get_version");
-        var getReceivedData = document.getElementById("get_received_data");
-        var getFirmwareVersion = document.getElementById("get_firmware_version");
-        var getLedStatus = document.getElementById("get_led_status");
-				var connectReader = document.getElementById("connectReader");
-        var getBatteryLevel = document.getElementById("get_battery_level");
-        var disconnectReader = document.getElementById("disconnectReader");
-
-        isReadyButton.addEventListener('click', function(){
-          ACR.isReady(
-            function(){alert('is Ready')},
-            function(){alert('is not Ready')}
-            );
-        });
-
-        getVersion.addEventListener('click', function(){
-          ACR.getVersion(_cb, _cb);
-        });
-
-        getFirmwareVersion.addEventListener('click', function(){
-          ACR.getFirmwareVersion(_cb, _cb);
-        });
-
-        getReceivedData.addEventListener('click', function(){
-          ACR.getReceivedData(_cb, _cb);
-        });
-
-        initNtagButton.addEventListener('click', function(){
-          ACR.initNTAG213(oldPasswordInput.value, passwordInput.value, _cb, _cb);
-        });
-
-        getLedStatus.addEventListener('click', function() {
-          ACR.getLedStatus(_cb, _cb);
-        });
-
-				connectReader.addEventListener('click', function() {
-		      ACR.connectReader(function(result){}, function(result){});
-			 	})
-        getBatteryLevel.addEventListener('click', function() {
-          ACR.getBatteryLevel(_cb, _cb);
-        });
-
-        disconnectReader.addEventListener('click', function() {
-          ACR.disconnectReader(_cb, _cb);
-        });
-
-        //var keyA = document.getElementById("key_a");
-        //var keyB = document.getElementById("key_b");
-        writeButton.addEventListener('click', function() {
-          //if(keyA.value.length > 0 || keyB.value.length > 0){
-            //if(keyA.value.length == 0){
-                //alert('please input KeyA');
-                //return;
-            //}
-            //if(keyB.value.length == 0){
-                //alert('please input KeyB');
-                //return;
-            //}
-            ////ACR.authenticateWithKeyB(keyB.value, function(){},function(){});
-            //ACR.writeAuthenticate(BLOCK,keyA.value,keyB.value, function(){
-              //ACR.writeData(BLOCK,writeInput.value,write_success,write_failure);
-            //}, function(){
-                //alert('write authenticate fail');
-            //});
-          //}else{
-            ACR.writeData(parseInt(dataBlock.value, 10), writeInput.value, passwordInput.value, write_success, write_failure);
-          //}
-        });
-        var disableButtons = function(){
-          readButton.disabled = true;
-          writeButton.disabled = true;
-          getVersion.disabled = true;
-          initNtagButton.disabled = true;
-        }
-        var enableButtons = function(){
-          readButton.disabled = false;
-          writeButton.disabled = false;
-          getVersion.disabled = false;
-          initNtagButton.disabled = false;
-        }
-        var success = function(result) {
-            alert("ATR: " + JSON.stringify(result));
-            //ACR.readData(4, passwordInput.value, read_uid_success, read_uid_failure);
-            enableButtons();
-        };
-
-        var failure = function(reason) {
-            alert("Error " + JSON.stringify(reason))
-        };
-
-        ACR.onCardAbsent = function() {
-            disableButtons();
-        };
-        console.log("Calling plugin");
-        ACR.start();
-        ACR.addTagListener(success, failure);
-        ACR.onReady = function (reader) {
-          pluginElement.innerHTML = "ready " + reader;
-        };
-        ACR.onAttach = function (device) {
-          pluginElement.innerHTML = "attched " + device;
-        };
-        ACR.onDetach = function (device) {
-          pluginElement.innerHTML = "detached " + device;
-        };
-        ACR.onBatteryLevelChange = function (level) {
-          batteryElement.innerHTML = "battery level " + level;
-        };
-        ACR.onBatteryLevelResult = function (level) {
-          alert("Current Battery Level:" + JSON.stringify(level));
-        };
-        console.log("Called plugin");
-        console.log('Received Event: ' + id);
+    var read_uid_success =  function(result){
+      alert("UID: " + JSON.stringify(result))
     }
+
+    var read_uid_failure =  function(result){
+      alert("error UID: " + JSON.stringify(result))
+    }
+    var read_success =  function(result){
+      var d = new Date();
+      read_result.innerHTML = read_result.innerHTML  + d.toLocaleString() + "&nbsp" + JSON.stringify(result) + "<br>";
+      //alert("data: " + JSON.stringify(result))
+    }
+
+    var read_failure =  function(result){
+      //alert("error data: " + JSON.stringify(result))
+      var d = new Date();
+      read_result.innerHTML = read_result.innerHTML  + d.toLocaleString() + "&nbsp" + JSON.stringify(result) + "<br>";
+    }
+    var _cb =  function(result){
+      alert("data: " + JSON.stringify(result))
+    }
+
+    var readButton = document.getElementById("read_data");
+    readButton.addEventListener('click', function() {
+      ACR.readData(4, passwordInput.value, read_success, read_failure);
+    });
+
+    var clearButton = document.getElementById("clear");
+
+    clearButton.addEventListener('click', function() {
+      read_result.innerHTML = "";
+    });
+
+    var passwordInput = document.getElementById("password_input");
+    var connectReader = document.getElementById("connectReader");
+
+    connectReader.addEventListener('click', function() {
+      ACR.connectReader(function(result){}, function(result){});
+    })
+
+    var disableButtons = function(){
+      readButton.disabled = true;
+    }
+    var enableButtons = function(){
+      readButton.disabled = false;
+    }
+    var success = function(result) {
+      //alert("ATR: " + JSON.stringify(result));
+      //ACR.readData(4, passwordInput.value, read_success, read_failure);
+      setTimeout(
+        function() {
+          ACR.readData(4, passwordInput.value, read_success, read_failure);
+        }, 1000
+      );
+      enableButtons();
+    };
+
+    var failure = function(reason) {
+      alert("Error " + JSON.stringify(reason))
+    };
+
+    ACR.onCardAbsent = function() {
+      disableButtons();
+    };
+    console.log("Calling plugin");
+    ACR.start();
+    var acrStatus = 'DETACH';
+    setInterval(
+      function(){
+        if(acrStatus == 'DETACH'){
+          ACR.connectReader(function(result){}, function(result){})
+        }
+      },  1000 );
+      ACR.addTagListener(success, failure);
+      ACR.onReady = function (reader) {
+        pluginElement.innerHTML = "ready " + reader;
+        acrStatus = 'READY';
+      };
+      ACR.onAttach = function (device) {
+        pluginElement.innerHTML = "attched " + device;
+        acrStatus = 'ATTACH';
+      };
+      ACR.onDetach = function (device) {
+        pluginElement.innerHTML = "detached " + device;
+        //setTimeout(ACR.connectReader(function(result){}, function(result){}), 500);
+        console.log("--------invoke-------");
+        acrStatus = 'DETACH';
+      };
+
+      console.log("Called plugin");
+      console.log('Received Event: ' + id);
+  }
 };
